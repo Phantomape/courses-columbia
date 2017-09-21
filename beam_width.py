3a558cbbb4eed763980fd8755396def4016ccba7
Xucheng <cxucheng@outlook.com>
2017-09-21 19:27:29 -0400

    minor revise

--- beam_width.py
+++ beam_width.py
@@ -1,13 +1,32 @@
 from gopigo import *
-
-dist = []
-idx = []
+import time
 
 enable_servo()
-for i in range(180):
-    tmp = us_dist(15)
-    if abs(tmp - dist[-1]) > 1:
-        idx.append(i)
-    dist.append(tmp)
-    
+servo(30)
+time.sleep(2)
+
+flag = True
+change = []
+prevDeg = 0
+prevDist = 0
+for currDeg in range(30, 150):
+    servo(currDeg)
+    currDist = us_dist(15)
+    print 'degree: ' + str(currDeg) + ', distance: ' + str(currDist)
+    if flag is True:
+        if prevDist - currDist > 50:
+            change.append(currDeg)
+            flag = False
+    else:
+        if currDist - prevDist > 50:
+            change.append(prevDeg)
+            flag = True
+
+    prevDeg = currDeg
+    prevDist = currDist
+
+if len(change) < 2:
+    print 'unsuccessful attempt'
+else:
+    print 'beam width: ' + str(change[1] - change[0])
 
